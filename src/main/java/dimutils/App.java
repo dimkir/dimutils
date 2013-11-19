eab492abaae161919d019715d3e7bd748874cdd0
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-11-19 14:32:16 +0000

    turned into maven project

---
+++ src/main/java/dimutils/App.java
@@ -0,0 +1,13 @@
+package dimutils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
